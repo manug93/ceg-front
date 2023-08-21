67ea108ef48c8fbad482a8316e5f4fa76dff1483
HUGUES ARMEL MAMA NKE <hugues.mama@campost.post>
2023-08-21 16:04:27 +0100

    Search

---
+++ pages/Search.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <Navbar></Navbar>
+        <Footer></Footer>
+    </div>
+
+</template>
+<script>
+export default{
+    name:"Search"
+}
+</script>
+<style>
+</style>
